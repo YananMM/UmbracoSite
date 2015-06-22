3c6b9b3d398bea5e3e0e03bcdb530f679ff259b1
YananMM <yananfun@outlook.com>
2015-06-22 21:24:25 +0800

    add contact us form test

---
+++ Controller/ContactUsSurfaceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleUmbraco.Model;
+
+namespace SimpleUmbraco.Controller
+{
+    public class ContactUsSurfaceController : Umbraco.Web.Mvc.SurfaceController
+    {
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.ActionName("ContactUsForm")]
+        public ActionResult ContactUsForm(ContactUsModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+            else
+            {
+                return CurrentUmbracoPage();
+
+            }
+        }
+    }
+}
---
+++ Model/ContactUsModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleUmbraco.Model
+{
+    public class ContactUsModel
+    {
+        [Required, Display(Name = "Enter your user name")]
+        public string Username { get; set; }
+        [Required, Display(Name = "Enter your phone number"), DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+    }
+}
